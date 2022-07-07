d4f86ee1bfca10ba55b82c31e8f947a838787bd7
mniam847 <mniam847@gmail.com>
2022-07-07 21:22:17 +0700

    admin invoice

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -1,29 +1,33 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Admin extends CI_Controller {
-    public function __construct(){
+class Admin extends CI_Controller
+{
+    public function __construct()
+    {
         parent::__construct();
-        if($this->session->userdata('status') != 'login'){
+        if ($this->session->userdata('status') != 'login') {
             redirect('login');
         }
     }
 
     // index
-    public function index(){
+    public function index()
+    {
         $data['user'] = $this->session->userdata('name');
         $data['totalproduct'] = count($this->multikarya->getAll("product"));
         $data['totalinvoice'] = count($this->multikarya->getAll("invoice"));
-        $tempVar = $this->multikarya->getData("invoice",array('status'=>1));
-        $data['percentinvoice'] = (count($tempVar)*100)/ count($this->multikarya->getAll("invoice"));
+        $tempVar = $this->multikarya->getData("invoice", array('status' => 1));
+        $data['percentinvoice'] = (count($tempVar) * 100) / count($this->multikarya->getAll("invoice"));
         $data['totalrecord'] = count($tempVar);
         $data['totalfeedback'] = count($this->multikarya->getAll("testimoni"));
-        $data['percentfeedback'] = (count($this->multikarya->getAll("testimoni"))*100)/count($tempVar);
+        $data['percentfeedback'] = (count($this->multikarya->getAll("testimoni")) * 100) / count($tempVar);
 
-		$this->load->view('admin/dashboard', $data);
-	}
+        $this->load->view('admin/dashboard', $data);
+    }
 
-    public function logout(){
+    public function logout()
+    {
         // $this->session->unset_userdata('status');
         $this->session->sess_destroy();
         redirect('login');
@@ -31,53 +35,108 @@ class Admin extends CI_Controller {
     // =========================================================================================================
 
     // Product
-    public function createPageProduct(){
+    public function createPageProduct()
+    {
         $data['user'] = $this->session->userdata('name');
-		$this->load->view('adminproduct/createpage', $data);
-	}
-    public function createProduct(){
+        $this->load->view('adminproduct/createpage', $data);
+    }
+    public function createProduct()
+    {
         $data['user'] = $this->session->userdata('name');
 
-        $submit_data = array(	'name' => $this->input->post('name'),
-                                'category' => $this->input->post('category'),
-                                'price' => $this->input->post('price'),
-								'describe' => $this->input->post('describe'),
-								'picture' => "default.jpg");
-        $this->multikarya->inputData("product",$submit_data);
+        $submit_data = array(
+            'name' => $this->input->post('name'),
+            'category' => $this->input->post('category'),
+            'price' => $this->input->post('price'),
+            'describe' => $this->input->post('describe'),
+            'picture' => "default.jpg"
+        );
+        $this->multikarya->inputData("product", $submit_data);
         redirect('admin/showProduct', 'refresh');
     }
-    public function showProduct(){
+    public function showProduct()
+    {
         $data['user'] = $this->session->userdata('name');
-        
-        $data['products'] = $this->multikarya->getAll("product");
-		$this->load->view('adminproduct/index', $data);
-	}
-    public function detailProduct($id){  //sekaligus update
-        $data['user'] = $this->session->userdata('name');
-        $submit_data = array( 'id' => $id);
-        $data['product'] = $this->multikarya->getData("product",$submit_data);
-        // var_dump($data['product']);
-		$this->load->view('adminproduct/detail', $data);
-	}
 
-    public function updateProduct(){
+        $data['products'] = $this->multikarya->getAll("product");
+        $this->load->view('adminproduct/index', $data);
+    }
+    public function detailProduct($id)
+    {  //sekaligus update
         $data['user'] = $this->session->userdata('name');
-    
-        $submit_data = array(	'name' => $this->input->post('name'),
-                                'category' => $this->input->post('category'),
-                                'price' => $this->input->post('price'),
-                                'describe' => $this->input->post('describe'),
-                                'picture' => $this->input->post('picture'),);
-        $index = array( 'id'=> $this->input->post('id')); 
+        $submit_data = array('id' => $id);
+        $data['product'] = $this->multikarya->getData("product", $submit_data);
+        // var_dump($data['product']);
+        $this->load->view('adminproduct/detail', $data);
+    }
+
+    public function updateProduct()
+    {
+        $data['user'] = $this->session->userdata('name');
+
+        $submit_data = array(
+            'name' => $this->input->post('name'),
+            'category' => $this->input->post('category'),
+            'price' => $this->input->post('price'),
+            'describe' => $this->input->post('describe'),
+            'picture' => $this->input->post('picture'),
+        );
+        $index = array('id' => $this->input->post('id'));
         // var_dump($submit_data);
         $this->multikarya->updateData("product", $submit_data, $index);
         redirect('admin/showProduct', 'refresh');
     }
-    public function deleteProduct($id){
-        $submit_data = array( 'id' => $id);
-        $this->multikarya->deleteData("product",$submit_data);
+    public function deleteProduct($id)
+    {
+        $submit_data = array('id' => $id);
+        $this->multikarya->deleteData("product", $submit_data);
         $data['user'] = $this->session->userdata('name');
-		redirect('admin/showProduct', 'refresh');
-	}
-    
+        redirect('admin/showProduct', 'refresh');
+    }
+
+    // ====================================================================================================
+    // Invoice
+
+    public function showInvoice()
+    {
+        $data['user'] = $this->session->userdata('name');
+
+        $data['invoice'] = $this->multikarya->getAll("invoice");
+        $this->load->view('admininvoice/index', $data);
+    }
+
+    public function detailInvoice($id)
+    {  //sekaligus update
+        $data['user'] = $this->session->userdata('name');
+        $submit_data = array('id' => $id);
+        $data['invoice'] = $this->multikarya->getData("invoice", $submit_data);
+        // var_dump($data['invoice']);
+        $this->load->view('admininvoice/detail', $data);
+    }
+
+    public function updateInvoice()
+    {
+        $data['user'] = $this->session->userdata('name');
+
+        $submit_data = array(
+            'name' => $this->input->post('name'),
+            'product' => $this->input->post('product'),
+            'message' => $this->input->post('message'),
+            'date_input' => $this->input->post('date_input'),
+            'dateline' => $this->input->post('dateline'),
+            'status' => $this->input->post('status'),
+            'price' => $this->input->post('price')
+        );
+        $index = array('id' => $this->input->post('id'));
+        // var_dump($submit_data);
+        $this->multikarya->updateData("invoice", $submit_data, $index);
+        redirect('admin/showInvoice', 'refresh');
+    }
+    public function deleteInvoice($id)
+    {
+        $submit_data = array('id' => $id);
+        $this->multikarya->deleteData("invoice", $submit_data);
+        $data['user'] = $this->session->userdata('name');
+        redirect('admin/showInvoice', 'refresh');
+    }
 }
---
+++ application/views/admininvoice/detail.php
@@ -0,0 +1,349 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Admin - Dashboard</title>
+    <link rel="icon" type="image/png" href="<?= base_url() ?>assets/logo/logo.png">
+
+    <!-- Custom fonts for this template-->
+    <link href="<?php echo base_url(); ?>assets/admin/aset/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href="<?php echo base_url(); ?>assets/admin/aset/fontawesome/css/fontawesome.css" rel="stylesheet">
+    <link href="<?php echo base_url(); ?>assets/admin/aset/fontawesome/css/brands.css" rel="stylesheet">
+    <link href="<?php echo base_url(); ?>assets/admin/aset/fontawesome/css/solid.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+
+    <!-- Custom styles for this template-->
+    <link href="<?php echo base_url(); ?>assets/admin/aset/css/sb-admin-2.min.css" rel="stylesheet">
+    <link href="<?php echo base_url(); ?>assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+
+</head>
+
+<body id="page-top">
+
+    <!-- Page Wrapper -->
+    <div id="wrapper">
+
+        <!-- Sidebar -->
+        <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+
+            <!-- Sidebar - Brand -->
+            <div class="sidebar-brand d-flex align-items-center justify-content-center ">
+                <div class="sidebar-brand-icon rotate-n-15">
+                    <a href="<?php echo base_url(); ?>"><img style="height: 50px; width:50px;" src="<?php echo base_url(); ?>assets/logo/logo.png" alt=""></a>
+                </div>
+                <div class="sidebar-brand-text mx-3">Admin</div>
+            </div>
+
+            <!-- Divider -->
+            <hr class="sidebar-divider my-0">
+
+            <!-- Nav Item - Dashboard -->
+            <li class="nav-item">
+                <a class="nav-link" href="<?php echo base_url(); ?>admin">
+                    <i class="fas fa-fw fa-tachometer-alt"></i>
+                    <span>Dashboard</span></a>
+            </li>
+
+            <!-- Divider -->
+            <hr class="sidebar-divider">
+
+            <!-- Heading -->
+            <div class="sidebar-heading">
+                Interface
+            </div>
+
+            <!-- Nav Item - Pages Collapse Menu -->
+            <!-- <li class="nav-item">
+                <a class="nav-link" href="<?php echo base_url(); ?>admin/showcontrol">
+                    <i class="fas fa-fw fa-cog"></i>
+                    <span>Pengaturan</span>
+                </a>
+            </li> -->
+
+            <li class="nav-item active">
+                <a class="nav-link" href="<?php echo base_url(); ?>admin/showProduct">
+                    <i class="fa-solid fa-database"></i>
+                    <span>Product</span>
+                </a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link" href="<?php echo base_url(); ?>admin/showinvoice">
+                    <i class="fa-solid fa-inbox"></i>
+                    <span>Invoice</span>
+                </a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link" href="<?php echo base_url(); ?>admin/showrecord">
+                    <i class="fa-solid fa-clock-rotate-left"></i>
+                    <span>Record</span>
+                </a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link" href="<?php echo base_url(); ?>admin/showportofolio">
+                    <i class="fa-solid fa-image"></i>
+                    <span>Portofolio</span>
+                </a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link" href="<?php echo base_url(); ?>admin/showtestimoni">
+                    <i class="fa-solid fa-comment-dots"></i>
+                    <span>Testimoni</span>
+                </a>
+            </li>
+
+
+            <!-- Divider -->
+            <hr class="sidebar-divider">
+
+            <!-- Heading -->
+            <div class="sidebar-heading">
+                Home
+            </div>
+
+            <!-- Nav Item - Charts -->
+            <li class="nav-item">
+                <a class="nav-link" href="<?php echo base_url(); ?>">
+                    <i class="fa-solid fa-house"></i>
+                    <span>Home</span>
+                </a>
+            </li>
+
+            <!-- Divider -->
+            <hr class="sidebar-divider d-none d-md-block">
+
+            <!-- Sidebar Toggler (Sidebar) -->
+            <div class="text-center d-none d-md-inline">
+                <button class="rounded-circle border-0" id="sidebarToggle"></button>
+            </div>
+
+        </ul>
+        <!-- End of Sidebar -->
+
+        <!-- Content Wrapper -->
+        <div id="content-wrapper" class="d-flex flex-column">
+
+            <!-- Main Content -->
+            <div id="content">
+
+                <!-- Topbar -->
+                <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
+
+                    <!-- Sidebar Toggle (Topbar) -->
+                    <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
+                        <i class="fa fa-bars"></i>
+                    </button>
+
+                    <!-- Topbar Search -->
+                    <form class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
+                        <div class="input-group">
+                            <input type="text" class="form-control bg-light border-0 small" placeholder="Search for..." aria-label="Search" aria-describedby="basic-addon2">
+                            <div class="input-group-append">
+                                <button class="btn btn-primary" type="button">
+                                    <i class="fas fa-search fa-sm"></i>
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+
+                    <!-- Topbar Navbar -->
+                    <ul class="navbar-nav ml-auto">
+
+                        <!-- Nav Item - Search Dropdown (Visible Only XS) -->
+                        <li class="nav-item dropdown no-arrow d-sm-none">
+                            <a class="nav-link dropdown-toggle" href="#" id="searchDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                <i class="fas fa-search fa-fw"></i>
+                            </a>
+                            <!-- Dropdown - Messages -->
+                            <div class="dropdown-menu dropdown-menu-right p-3 shadow animated--grow-in" aria-labelledby="searchDropdown">
+                                <form class="form-inline mr-auto w-100 navbar-search">
+                                    <div class="input-group">
+                                        <input type="text" class="form-control bg-light border-0 small" placeholder="Search for..." aria-label="Search" aria-describedby="basic-addon2">
+                                        <div class="input-group-append">
+                                            <button class="btn btn-primary" type="button">
+                                                <i class="fas fa-search fa-sm"></i>
+                                            </button>
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
+                        </li>
+
+                        <!-- Nav Item - User Information -->
+                        <li class="nav-item dropdown no-arrow">
+                            <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                <span class="mr-2 d-none d-lg-inline text-gray-600 small"><b><?= $user ?></b></span>
+                                <img class="img-profile rounded-circle" src="<?php echo base_url(); ?>assets/admin/aset/img/undraw_profile.svg">
+                            </a>
+                            <!-- Dropdown - User Information -->
+                            <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
+                                <a class="dropdown-item" href="#">
+                                    <i class="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400"></i>
+                                    Settings
+                                </a>
+                                <div class="dropdown-divider"></div>
+                                <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
+                                    <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
+                                    Logout
+                                </a>
+                            </div>
+                        </li>
+
+                    </ul>
+
+                </nav>
+                <!-- End of Topbar -->
+
+                <!-- Begin Page Content -->
+                <div class="container-fluid">
+
+                    <!-- Page Heading -->
+                    <h1 class="h3 mb-2 text-gray-800">Data Invoice</h1>
+                    <p class="mb-4">Data yang menampilkan seluruh invoice yang dimiliki</a>.</p>
+
+                    <!-- DataTales Example -->
+                    <div class="card shadow mb-4">
+                        <div class="card-header py-3 grid" style="--bs-columns: 10; --bs-gap: 1rem;">
+                            <h6 class="m-0 font-weight-bold text-primary g-col-6">DataInvoice</h6>
+                        </div>
+                        <div class="card-body">
+                            <div class="row">
+                                <form action="<?= base_url('admin/updateInvoice'); ?>" method="post">
+                                    <div class="row">
+                                        <div class="col-8">
+                                            <div class="form-group row">
+                                                <label for="0" class="col-sm-2  col-form-label">Id Invoice</label>
+                                                <div class="col-sm-5">
+                                                    <input type="text" value="<?= $invoice[0]['id']; ?>" class="form-control" id="0" name="id" placeholder="Tulis nama produk" readonly>
+                                                </div>
+                                            </div>
+                                            <div class="form-group row">
+                                                <label for="1" class="col-sm-2  col-form-label">Nama Invoice</label>
+                                                <div class="col-sm-5">
+                                                    <input type="text" value="<?= $invoice[0]['name']; ?>" class="form-control" id="1" name="name" placeholder="Tulis nama produk" autocomplete="off" readonly>
+                                                </div>
+                                            </div>
+                                            <div class="form-group row">
+                                                <label for="2" class="col-sm-2  col-form-label">Product</label>
+                                                <div class="col-sm-5">
+                                                    <input type="text" value="<?= $invoice[0]['product']; ?>" class="form-control" id="2" name="product" placeholder="Tulis kategori produk" autocomplete="off" readonly>
+                                                </div>
+                                            </div>
+                                            <div class="form-group row">
+                                                <label for="3" class="col-sm-2  col-form-label">Message</label>
+                                                <div class="col-sm-5">
+                                                    <input type="text" value="<?= $invoice[0]['message']; ?>" class="form-control" id="3" name="message" placeholder="Masukkan harga produk" autocomplete="off" readonly>
+                                                </div>
+                                            </div>
+                                            <div class="form-group row">
+                                                <label for="4" class="col-sm-2  col-form-label">Tanggal Pesanan</label>
+                                                <div class="col-sm-5">
+                                                    <input type="text" value="<?= $invoice[0]['date_input']; ?>" class="form-control" id="3" name="date_input" placeholder="Masukkan keterangan" autocomplete="off" readonly>
+                                                </div>
+                                            </div>
+                                            <div class="form-group row">
+                                                <label for="4" class="col-sm-2  col-form-label">Tanggal Berakhir</label>
+                                                <div class="col-sm-5">
+                                                    <input type="text" value="<?= $invoice[0]['dateline']; ?>" class="form-control" id="3" name="dateline" placeholder="Masukkan keterangan" autocomplete="off" readonly>
+                                                </div>
+                                            </div>
+                                            <div class="form-group row">
+                                                <label for="4" class="col-sm-2  col-form-label">Harga</label>
+                                                <div class="col-sm-5">
+                                                    <input type="text" value="<?= $invoice[0]['price']; ?>" class="form-control" id="3" name="price" placeholder="Masukkan keterangan" autocomplete="off" readonly>
+                                                </div>
+                                            </div>
+                                            <div class="form-group row">
+                                                <label for="4" class="col-sm-2  col-form-label">Status</label>
+                                                <div class="col-sm-5">
+                                                    <input type="text" value="<?= $invoice[0]['status']; ?>" class="form-control" id="3" name="status" placeholder="Masukkan keterangan" autocomplete="off">
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <a href="<?= base_url('admin/showInvoice'); ?>" class="btn btn-info">Batal</a>
+                                    <a href="<?= base_url('admin/deleteInvoice'); ?>" class="btn btn-danger">delete</a>
+                                    <button type="submit" name="submit" class="btn btn-primary">Update</button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+                <!-- /.container-fluid -->
+
+            </div>
+            <!-- End of Main Content -->
+
+            <!-- Footer -->
+            <footer class="sticky-footer bg-white">
+                <div class="container my-auto">
+                    <div class="copyright text-center my-auto">
+                        <span>Copyright &copy; Multikarya 2022</span>
+                    </div>
+                </div>
+            </footer>
+            <!-- End of Footer -->
+
+        </div>
+        <!-- End of Content Wrapper -->
+
+    </div>
+    <!-- End of Page Wrapper -->
+
+    <!-- Scroll to Top Button-->
+    <a class="scroll-to-top rounded" href="#page-top">
+        <i class="fas fa-angle-up"></i>
+    </a>
+
+    <!-- Logout Modal-->
+    <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
+                    <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">×</span>
+                    </button>
+                </div>
+                <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
+                <div class="modal-footer">
+                    <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
+                    <a class="btn btn-primary" href="<?php echo base_url(); ?>admin/logout">Log out</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Bootstrap core JavaScript-->
+    <script src="<?php echo base_url(); ?>assets/admin/aset/vendor/jquery/jquery.min.js"></script>
+    <script src="<?php echo base_url(); ?>assets/admin/aset/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Core plugin JavaScript-->
+    <script src="<?php echo base_url(); ?>assets/admin/aset/vendor/jquery-easing/jquery.easing.min.js"></script>
+
+    <!-- Custom scripts for all pages-->
+    <script src="<?php echo base_url(); ?>assets/admin/aset/js/sb-admin-2.min.js"></script>
+    <script src="<?php echo base_url(); ?>assets/bootstap/js/botstrap.min.js"></script>
+
+    <!-- Page level plugins -->
+    <script src="<?php echo base_url(); ?>assets/admin/aset/vendor/chart.js/Chart.min.js"></script>
+
+    <!-- Page level custom scripts -->
+    <script src="<?php echo base_url(); ?>assets/admin/aset/js/demo/chart-area-demo.js"></script>
+    <script src="<?php echo base_url(); ?>assets/admin/aset/js/demo/chart-pie-demo.js"></script>
+
+</body>
+
+</html>
