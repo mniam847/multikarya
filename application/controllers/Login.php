05b571792fe66aea31fdc9b44d2ca1677e2212ab
mniam847 <mniam847@gmail.com>
2022-07-07 08:41:46 +0700

    ubah redirect login

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -1,47 +1,51 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Login extends CI_Controller {
+class Login extends CI_Controller
+{
 
-	public function __construct(){
+    public function __construct()
+    {
         parent::__construct();
-        if($this->session->userdata('status') == "login" ){
-            $tempVar = $this->multikarya->getData("user",array('name'=>$this->session->userdata('name')));
+        if ($this->session->userdata('status') == "login") {
+            $tempVar = $this->multikarya->getData("user", array('name' => $this->session->userdata('name')));
             $tempVar = $tempVar[0]["role"];
             redirect('admin');
         }
     }
 
-	public function index(){
-		$this->load->view('auth/login');
-	}
+    public function index()
+    {
+        $this->load->view('auth/login');
+    }
 
-	//validasi login
-	public function validate(){
-		$user = $this->input->post('username');
+    //validasi login
+    public function validate()
+    {
+        $user = $this->input->post('username');
         $pass = $this->input->post('password');
 
         $dataPenunjuk = array(
-            'name'=>$user,
-            'password'=>$pass
+            'name' => $user,
+            'password' => $pass
         );
 
         $dataUser = count($this->multikarya->getLogin($dataPenunjuk));
-        
-		// echo $dataUser;
-        if($dataUser==1){
+
+        // echo $dataUser;
+        if ($dataUser == 1) {
             // $accountUser = $this->scsc->getData("user",$dataPenunjuk);
             $accountSession = array(
-                'name'=>$user,
-                'status'=>'login'
+                'name' => $user,
+                'status' => 'login'
             );
-            
-            
+
+
             $this->session->set_userdata($accountSession);
-            redirect('home');
-        }else{
+            redirect('admin');
+        } else {
             $this->session->set_flashdata('message', '<div class="alert alert-danger text-center">Data salah input, coba lagi!</div>');
             redirect('login');
         }
-	}
+    }
 }
