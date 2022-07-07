f30aa2a74895fe2a95a0e13c14220d9b5d99761e
Wahyu Rochman <wrochcode@gmail.com>
2022-07-07 10:16:06 +0700

    Update Model

--- application/models/multikarya.php
+++ application/models/multikarya.php
@@ -9,27 +9,33 @@ class multikarya extends CI_Model
     }
     
     //tampil semua data
-    public function getAll($tabel)
-    {
+    public function getAll($tabel){
         return $this->db->get($tabel)->result_array();
     }
 
     //tampil satu data
-    public function getData($dataBase, $where)
-    {
+    public function getData($dataBase, $where){
         return $this->db->get_where($dataBase, $where)->result_array();
     }
 
     // tampil beberapa
-    public function getFew($tabel, $short, $limit)
-    {
+    public function getFew($tabel, $short, $limit){
         $this->db->order_by('id', $short);
         return $this->db->get($tabel,0,$limit)->result_array();
     }
 
     //inputdata
-    public function inputData($things, $database)
-    {
+    public function inputData($things, $database){
         return $this->db->insert($database, $things);
     }
+
+    //update data
+    public function updateData($tabel, $data, $where){
+        return $$this->db->update($tabel, $data, $where);
+    }
+
+    //delete data
+    public function Delete($table, $where){
+        return $this->db->delete($table, $where);
+    }    
 }
