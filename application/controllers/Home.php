68f70ced237d4d2beb33702e3d8b32ca22fce110
Wahyu Rochman <wrochcode@gmail.com>
2022-07-07 11:17:04 +0700

    update all

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -7,21 +7,21 @@ class Home extends CI_Controller
     public function index(){
         // template
         // ------------------------------------------------------------------------------------------------------------
-        // $tempVar = $this->multikarya->getData("about",array('name'=>'foreword'));
-        // $tempVar = $tempVar[0]["value"];
-        // $data['foreword'] = $tempVar;
+        $tempVar = $this->multikarya->getData("about",array('name'=>'foreword'));
+        $tempVar = $tempVar[0]["value"];
+        $data['foreword'] = $tempVar;
         
-        // $tempVar = $this->multikarya->getData("about",array('name'=>'telp'));
-        // $tempVar = $tempVar[0]["value"];
-        // $data['telp'] = $tempVar;
+        $tempVar = $this->multikarya->getData("about",array('name'=>'telp'));
+        $tempVar = $tempVar[0]["value"];
+        $data['telp'] = $tempVar;
         
-        // $tempVar = $this->multikarya->getData("about",array('name'=>'email'));
-        // $tempVar = $tempVar[0]["value"];
-        // $data['email'] = $tempVar;
+        $tempVar = $this->multikarya->getData("about",array('name'=>'email'));
+        $tempVar = $tempVar[0]["value"];
+        $data['email'] = $tempVar;
         
-        // $tempVar = $this->multikarya->getData("about",array('name'=>'address'));
-        // $tempVar = $tempVar[0]["value"];
-        // $data['address'] = $tempVar;
+        $tempVar = $this->multikarya->getData("about",array('name'=>'address'));
+        $tempVar = $tempVar[0]["value"];
+        $data['address'] = $tempVar;
         // ------------------------------------------------------------------------------------------------------------
         
         $data['product'] = $this->multikarya->getAll("product");
