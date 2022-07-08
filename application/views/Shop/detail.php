2131d2862ee7270a6874cbc67eba4d1ec8ebb021
Wahyu Rochman <wrochcode@gmail.com>
2022-07-09 01:24:58 +0700

    update kesekian kalinya jancokkkkk

--- application/views/Shop/detail.php
+++ application/views/Shop/detail.php
@@ -117,7 +117,7 @@
 
     <!-- Page Header Start -->
     <div class="container-fluid bg-secondary mb-5">
-        <div class="d-flex flex-column align-items-center justify-content-center" style="min-height: 300px">
+        <div class="d-flex flex-column align-items-center justify-content-center" style="min-height: 200px">
             <h1 class="font-weight-semi-bold text-uppercase mb-3">Product Detail</h1>
             <div class="d-inline-flex">
                 <p class="m-0"><a href="">Home</a></p>
