b71efbbd03561736180e1baea04610179194fd86
Wahyu Rochman <wrochcode@gmail.com>
2022-07-08 15:55:08 +0700

    update

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -4,6 +4,14 @@ defined('BASEPATH') or exit('No direct script access allowed');
 class Admin extends CI_Controller
 {
 
+    public function __construct()
+    {
+        parent::__construct();
+        if (!$this->session->userdata('status') == "login") {
+            redirect('login');
+        }
+    }
+
     // index
     public function index()
     {
