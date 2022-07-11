f783889d6a32e545b34077fe98f0613715df4f63
Wahyu Rochman <wrochcode@gmail.com>
2022-07-11 11:00:33 +0700

    update

--- ardian.sql
+++ ardian.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 08, 2022 at 08:15 PM
+-- Generation Time: Jul 10, 2022 at 04:57 PM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.1.6
 
